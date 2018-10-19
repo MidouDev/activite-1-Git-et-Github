09d7938faf1c3a7a682a920dc7a54c72d7cb62d9
MidouDev <htmidou@gmail.com>
2018-10-19 15:25:53 +0100

    modification du bouton dans App.html et App.js

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 var bouton = document.getElementById("bouton");
 var couleur = "white";
-bouton.addEventListener("click",function(){
+bouton.addEventListener("click",function(e){
 	if(couleur==="white"){
+		e.target.innerHTML="Blanche ?"
 		couleur="yellow"
 		document.body.style.backgroundColor = couleur;
 	}else{
+		e.target.innerHTML="Jaune ?"
 		couleur="white"
 		document.body.style.backgroundColor = couleur;
 	}
